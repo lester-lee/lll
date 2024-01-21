9f4e5b6e692074f872e4984e2a2ebfaee427de9a
Lester Lee <contact@lester-lee.com>
2024-01-21 15:58:24 -0500

    Remove reference fn

--- sketch.js
+++ sketch.js
@@ -85,22 +85,3 @@ function drawFace() {
     drawHand(positions.rightHand.x, positions.rightHand.y);
   });
 }
-
-// A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
-    const pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j += 1) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      const keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
-    }
-  }
-}
